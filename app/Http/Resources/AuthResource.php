54d82b23dd201955c640cfbb8abfb028f12defbe
zobirofkir <zobirofkir19@gmail.com>
2024-12-12 20:18:53 +0100

    return name in auth resource

--- app/Http/Resources/AuthResource.php
+++ app/Http/Resources/AuthResource.php
@@ -16,6 +16,7 @@ class AuthResource extends JsonResource
     {
         return [
             "id" => $this->id,
+            "name" => $this->name,
             "email" => $this->email,
             "accessToken" => $this->generateToken()
         ];
